3663f3ca56641fbc07ee4a78a69396965d0a1c8c
Vitalii <vitalii.kutsenko89@gmail.com>
2021-12-02 21:42:25 +0300

    refresh style

--- style.js
+++ style.js
@@ -11,3 +11,28 @@ style("language");
 style("skills");
 style("employments");
 style("books");
+const ar = document.querySelector(".frame img");
+
+const links = {
+    mail: "./src/icon/email.png",
+    number: " ./src/icon/smartphone.png",
+    location: "./src/icon/map.png",
+    linkedin: "./src/icon/linkedin.png",
+    github: "./src/icon/github.png",
+};
+const list = document.querySelectorAll(".contact li img");
+console.dir(list.dataset);
+document.body.onload = () => {
+    ar.src = "./src/photo/photo.png";
+    [...list].forEach((item) => {
+        if (item.dataset.contact in links) {
+            item.src = links[item.dataset.contact];
+            // console.log(item.children);
+        }
+        // console.log(item.dataset.contact);
+    });
+    // console.dir(document.body);
+};
+//
+
+console.log(ar.src);
