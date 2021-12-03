322bbfc8b80cf741c09e46d4d5b678d6aacd48b8
Vitalii <vitalii.kutsenko89@gmail.com>
2021-12-03 17:50:16 +0300

    alpha resume

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import location from "./icon/map.png";
 import linkedin from "./icon/linkedin.png";
 import github from "./icon/github.png";
 import photo from "./photo/photo.png";
+import ipris from "./icon/ipris.png";
+import smt from "./icon/smt.png";
 import "./style.css";
 const ar = document.querySelector(".frame img");
 
@@ -15,13 +17,18 @@ const links = {
     github: github,
 };
 const list = document.querySelectorAll(".contact li img");
+const ltds = document.querySelectorAll("[data-ltd]");
+
 document.body.onload = () => {
     ar.src = photo;
+    [...ltds].forEach((icon) => {
+        if (icon.dataset.ltd in { ipris, smt }) {
+            icon.src = { ipris, smt }[icon.dataset.ltd];
+        }
+    });
     [...list].forEach((item) => {
         if (item.dataset.contact in links) {
             item.src = links[item.dataset.contact];
         }
     });
 };
-
-console.log(window);
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const copyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const { UnusedFilesWebpackPlugin } = require("unused-files-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = {
     context: path.resolve(__dirname, "src"),
     entry: {
         main: "./app.js",
     },
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+    devServer: {
+        static: "./dist",
+        hot: true,
+        port: 5000,
+        open: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
     plugins: [
+        // new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             title: "Caching",
             template: path.resolve(__dirname, "./index.html"),
@@ -18,30 +36,41 @@ module.exports = {
         new copyWebpackPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, "src/icon/favicon.ico"),
+                    from: path.resolve(__dirname, "./src/icon/favicon.ico"),
                     to: path.resolve(__dirname, "dist"),
                 },
             ],
         }),
         // new UnusedFilesWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: "bundle.[hash].css",
+            // filename: "bundle.[hash].css",
+            filename: "style.css",
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [
+                    "css-hot-loader",
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ],
             },
             {
                 test: /\.(png|svg|ico|jpeg)$/i,
-                use: ["file-loader"],
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
             },
         ],
-    },
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
     },
 };
